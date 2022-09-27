a5101d1e1b9cdfd7796a7aa88daea7070dfd0c4b
iamonuwa <me@iamonuwa.dev>
2022-09-27 11:35:00 +0100

    fix(lint): resolve lint on dapp

--- packages/dapp/src/components/Table.tsx
+++ packages/dapp/src/components/Table.tsx
@@ -15,13 +15,13 @@ const Table: FC<TableProps> = ({ columns, data, onRowSelect }) => {
     <table className="min-w-full divide-y divide-gray-300" {...getTableProps()}>
       <thead className="text-sm">
         {headerGroups.map((headerGroup, i) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
+          <tr key={i}>
             {headerGroup.headers.map((column, j) => {
               return (
                 <th
                   scope="col"
                   className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-                  {...column.getHeaderProps()}
+                  key={j}
                 >
                   {column.render("Header")}
                 </th>
@@ -31,16 +31,13 @@ const Table: FC<TableProps> = ({ columns, data, onRowSelect }) => {
         ))}
       </thead>
       <tbody className="divide-y divide-gray-200 bg-white" {...getTableBodyProps()}>
-        {rows.map((row, i) => {
+        {rows.map((row, idx) => {
           prepareRow(row);
           return (
-            <tr {...row.getRowProps()} onClick={() => onRowSelect && onRowSelect(row)}>
-              {row.cells.map((cell, j) => {
+            <tr key={idx} onClick={() => onRowSelect && onRowSelect(row)}>
+              {row.cells.map((cell, jdx) => {
                 return (
-                  <td
-                    className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6"
-                    {...cell.getCellProps()}
-                  >
+                  <td key={jdx} className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
                     {cell.render("Cell")}
                   </td>
                 );
